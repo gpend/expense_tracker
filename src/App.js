1a5d526941c8da754b94fcb60a58fa3c6350a404
Greg Pendleton <gpendleton@gmail.com>
2022-02-20 10:37:02 -0600

    added basic logic and expense table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [expenses, setExpenses] = React.useState([])
+  const [currentExpense, setCurrentExpense] = React.useState({
+                                                      description: "",
+                                                      amount: "",
+                                                      date: ""
+                                                    })
+
+  function trackExpense(event){
+    switch (event.target.name){
+      case 'expense_description':
+        setCurrentExpense(prev => ({...prev,
+          description:event.target.value}));
+        break;
+      case 'expense_amount':
+        setCurrentExpense(prev => ({...prev,
+          amount:event.target.value}))
+        break;
+      case 'expense_date':
+        setCurrentExpense(prev => ({...prev,
+          date:event.target.value}));
+        break;
+      default: 
+    }
+  }
+  
+  function addExpense(event){
+    event.preventDefault()
+    setExpenses(prev => ([...prev,currentExpense]))
+    setCurrentExpense({
+      description: "",
+      amount: "",
+      date: ""
+    })
+  }
+
+  function ListExpenses(){
+    return(
+      expenses.map(function(expense){ 
+        return (
+        <tr>
+          <td>{expense.description}</td>
+          <td>{expense.amount}</td>
+          <td>{expense.date}</td>
+        </tr>
+      )})
+    )
+  }
+
   return (
     <div className="App">
       <header>
@@ -14,13 +63,40 @@ function App() {
             <p>date</p>
           </div>
           <form>
-            <input className='expense-description' name='expense-description' />
-            <input className='expense-amount' name='expense-amount' />
-            <input className='expense-date' name='expense-date' type={'date'} />
-            <button>➕</button>
+            <input 
+              className='expense_description' 
+              name='expense_description' 
+              value={currentExpense.description}
+              onChange={trackExpense}
+            />
+            <input 
+              className='expense_amount' 
+              name='expense_amount' 
+              value={currentExpense.amount}
+              onChange={trackExpense}
+            />
+            <input 
+              className='expense_date' 
+              name='expense_date' 
+              type={'date'} 
+              value={currentExpense.date}
+              onChange={trackExpense}
+            />
+            <button onClick={addExpense}>➕</button>
             <button>🗑️</button>      
           </form>
         </div>
+        <table>
+          <tbody>
+            <tr>
+              <th>description</th>
+              <th>amount</th>
+              <th>date</th>
+            </tr>
+            <ListExpenses />
+          </tbody>
+        </table>
+        
       </main>
     </div>
   );
